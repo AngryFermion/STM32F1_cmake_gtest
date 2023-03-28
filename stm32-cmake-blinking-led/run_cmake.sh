0f4f24bc2997ce93aeaab6e7130ad5656bb5edc3
AngryFermion <vinnooh@gmail.com>
2023-03-28 17:19:58 +0530

    script

--- stm32-cmake-blinking-led/run_cmake.sh
+++ stm32-cmake-blinking-led/run_cmake.sh
@@ -65,7 +65,8 @@ then
  ISS_NUM=$(awk '{print $1}' issue_text.tx)
  cd ..
  cd ..
- cd /STM32F1_cmake_gtest
+ pwd
+ cd STM32F1_cmake_gtest/
  gh issue close ${ISS_NUM}
  echo "Building and FLashing STM32F1xx board....."
  cd ..
