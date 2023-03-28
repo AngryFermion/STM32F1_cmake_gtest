27fdc379b5263ae5e5919bd9d92409a98fb10898
AngryFermion <vinnooh@gmail.com>
2023-03-28 18:19:05 +0530

    script

--- stm32-cmake-blinking-led/run_cmake.sh
+++ stm32-cmake-blinking-led/run_cmake.sh
@@ -69,6 +69,7 @@ then
  pwd
  cd STM32F1_cmake_gtest/
 # echo "ancit12345" | sudo -S gh issue close ${ISS_NUM}
+ git config --global --add safe.directory ../
  gh issue close ${ISS_NUM}
  echo "Building and FLashing STM32F1xx board....."
  cd ..
