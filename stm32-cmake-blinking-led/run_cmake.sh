cf26c91746e717e29b171bb60abf85fa9beed5bf
AngryFermion <vinnooh@gmail.com>
2023-03-29 11:32:39 +0530

    Intentional error made in main.c for testing purposes.

--- stm32-cmake-blinking-led/run_cmake.sh
+++ stm32-cmake-blinking-led/run_cmake.sh
@@ -70,7 +70,7 @@ comment
  pwd
 # cd STM32F1_cmake_gtest/
  #git config --global --add safe.directory /var/lib/jenkins/STM32F1_cmake_gtest
- echo "Building and Flashing STM32F1xx board....."
+ echo "Building and Flashing STM32F1xx board with the .elf file....."
  #cd ..
  #cd ..
  #cd stm32-cmake-blinking-led/
