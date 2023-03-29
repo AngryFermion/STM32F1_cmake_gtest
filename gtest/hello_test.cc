2eaedec6b19884255737abca2b0048327d2be7e4
AngryFermion <vinnooh@gmail.com>
2023-03-29 14:34:21 +0530

    Changes made to test Gtest.

--- gtest/hello_test.cc
+++ gtest/hello_test.cc
@@ -16,7 +16,7 @@ TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
   gpio_test = init_led();
   printf("Pin_number:%d",gpio_test[0]);
-  EXPECT_EQ(gpio_test[0], GPIO_PIN_5);
+  EXPECT_EQ(gpio_test[0], GPIO_PIN_6);
   EXPECT_EQ(gpio_test[1], GPIO_MODE_OUTPUT_PP);
   EXPECT_EQ(gpio_test[2], GPIO_NOPULL);
   EXPECT_EQ(gpio_test[3], GPIO_SPEED_FREQ_LOW);
--- stm32-cmake-blinking-led/run_cmake.sh
+++ stm32-cmake-blinking-led/run_cmake.sh
@@ -33,26 +33,26 @@
 
 
 
-#cd ..
-#rm -r build/
-#pwd
-#mkdir build
-#cd build
-#cmake ..
-#cd ..
-#cd build/testfiles/
-#make
-#cd ..
-#cd ..
+cd ..
+rm -r build/
+pwd
+mkdir build
+cd build
+cmake ..
+cd ..
+cd build/testfiles/
+make
+cd ..
+cd ..
 
-#cmake -S . -B build
-#cmake --build build
-#pwd
-#cd build/
-#cd gtest/
-#make
-#pwd
-#ctest --output-on-failure
+cmake -S . -B build
+cmake --build build
+pwd
+cd build/
+cd gtest/
+make
+pwd
+ctest --output-on-failure
 #TEST_RET=$?  
 #echo "Test has returned:"${TEST_RET}
 #if [ ${TEST_RET} == 0 ]
@@ -66,25 +66,14 @@
  #cd ..
  #cd ..
 # cd ..
- pwd
- set -e
-# cd STM32F1_cmake_gtest/
- #git config --global --add safe.directory /var/lib/jenkins/STM32F1_cmake_gtest
- echo "Building and Flashing STM32F1xx board with code 12:41....."
- #cd ..
- #cd ..
- #cd stm32-cmake-blinking-led/
- rm -r build/
- mkdir build
- cd build/
- cmake ../
- cmake --build .
- cmake --build . --target stm32-flash
- echo "Flashining ended..."
-#else
-# echo "GTEST NOT PASSED..."
-# echo "CREATING AN ISSUE ON GIT REPOSITORY"
- #gh issue create -a "@me" -b "Gtest failed. Please check GPIO configuration." -t "GPIO Tests"
- #ISS_NUM=${ISS_NUM} + 1
- #gh issue list > ../../Issues/issue_text.tx
-#fi
+ ##Here: pwd
+ #set -e
+ #echo "Building and Flashing STM32F1xx board with code 12:41....."
+ #rm -r build/
+ #mkdir build
+ #cd build/
+ #cmake ../
+ #cmake --build .
+ #cmake --build . --target stm32-flash
+ #echo "Flashining ended..."
+
