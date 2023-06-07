14443baa282e4945aee15811350bcaf81740c4d0
AngryFermion <vinnooh@gmail.com>
2023-06-07 15:37:55 +0530

    Demo -1

--- stm32-cmake-blinking-led/source/main/src/main.c
+++ stm32-cmake-blinking-led/source/main/src/main.c
@@ -112,13 +112,13 @@ int main(void)
 
         HAL_Delay(50);
 
-    //	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,GPIO_PIN_SET);
+    	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_SET);
 	
-//	HAL_Delay(500);
+	HAL_Delay(500);
         
-//	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_RESET);
 
-  //      HAL_Delay(500);
+       HAL_Delay(500);
  
 	
     /* USER CODE BEGIN 3 */
